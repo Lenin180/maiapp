23285a698e4d5f3c6c61e7ab479d959bbf8b6159
Lenin180 <112209813+Lenin180@users.noreply.github.com>
2023-06-24 00:26:28 -0600

    get creditos y parte del front

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -20,12 +20,27 @@ export class InicioPage implements OnInit {
   first_lastname!: string;
   second_lastname!: string;
   admission!: string;
-  
+  firstLetter2!: string;
+  firstLetter!: string;
+  access!: string;
+  id!: string;
+  responseData: any;
   constructor(private router: Router, private http:HttpClient, private cookieService: CookieService ) { 
-
-   
+    
+    
   }
+  ionViewDidEnter() {
+    // Llama al método que deseas ejecutar siempre al mostrar la página
+    this.getcookies();
+    this.genpicture();
+    this.getcreditos();
+
+  }
+
+
     getcookies(){
+      this.id = this.cookieService.get('user_id');
+      //console.log('user_id', this.id);
       this.fullname = this.cookieService.get('fullnameU');
       this.emailU = this.cookieService.get('emailU');
       this.occupation = this.cookieService.get('Ocupacion');
@@ -35,18 +50,49 @@ export class InicioPage implements OnInit {
       this.first_lastname = this.cookieService.get('Ape1');
       this.second_lastname = this.cookieService.get('Ape2');
       this.admission = this.cookieService.get('Ingreso');
+      localStorage.getItem('access_token');
+      
+      //this.access = this.cookieService.get('access_tokenk');
+      //console.log('access_tokenk', this.access);
+      
 
     //console.log('Nombre estudiante', this.fullname);
-    console.log('Email estudiante', this.emailU);
+    //console.log('Email estudiante', this.emailU);
   
+    }
+
+    genpicture(){
+      const firstLetter = this.full_name.charAt(0);
+      //console.log('Primera letra:', firstLetter);
+      const firstLetter2 = this.first_lastname.charAt(0);
+      //console.log('Primera letra:', firstLetter2);
     }
   
   
 
-  ngOnInit() {
-    this.getcookies();
+  ngOnInit() {}
+  
+  getcreditos() {
+    const url = `https://platform-api.aaaimx.org/api/v1/complementaryCredit/credits/${this.id}`;
 
-  } 
+    // Define los encabezados de la solicitud
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+    });
+
+    this.http.get(url, { headers }).subscribe(
+      (response: any) => {
+        // Lógica para manejar la respuesta exitosa
+        console.log(response);
+        this.responseData = response;
+      },
+      (error) => {
+        // Lógica para manejar el error
+        console.error(error);
+      }
+    );
+    }
+  
    //Boton bancode proyectos 
   redirectToUrl() {
     const url = 'https://docs.google.com/spreadsheets/d/1vTdiAxo60aoPjZwX-IprmwO3fc8sOOVu2zeqUzYXo4I/edit#gid=0'; // URL a la que deseas redireccionar
@@ -69,7 +115,7 @@ export class InicioPage implements OnInit {
     }
   logout() {
     // Redirigir a la página de inicio de sesión
-    this.cookieService.deleteAll();
+   // this.cookieService.deleteAll();
     this.router.navigateByUrl('/home');
   }
 
